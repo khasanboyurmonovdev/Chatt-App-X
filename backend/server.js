b89c6a2813ba5be14ffc2ae0c01cbbb8908adc25
khasanboyurmonovdev <khasanboyurmonov1998@yahoo.com>
2024-11-05 09:10:39 +0900

    feat: Password is hashed with bcrypt and authcontroller business logic created

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,16 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectToMnogoDB from "./db/connectToMongoDB.js";
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+app.use(express.json()); //to parse the incoming requests with JSON payloads from
 
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
   //root route http://localhost:5000/
   res.send("Hello World!!!");
-});
+});  */
 app.use("/api/auth", authRoutes);
-
 app.listen(PORT, () => {
   connectToMnogoDB();
   console.log(`Server Running on port ${PORT}`);
