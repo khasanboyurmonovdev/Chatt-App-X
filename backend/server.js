9e5b73c7e1527ff13c718ea4d82d849c48a2849c
khasanboyurmonovdev <khasanboyurmonov1998@yahoo.com>
2024-11-07 06:38:48 +0900

    feat: developed middleware and modelSchema

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMnogoDB from "./db/connectToMongoDB.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); //to parse the incoming requests with JSON payloads from
+app.use(cookieParser());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 /*app.get("/", (req, res) => {
   //root route http://localhost:5000/
   res.send("Hello World!!!");
 });  */
-app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
   connectToMnogoDB();
   console.log(`Server Running on port ${PORT}`);
