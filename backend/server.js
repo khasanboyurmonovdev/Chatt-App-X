9818c5224acdfa47ae11a599a244e65102c890e4
khasanboyurmonovdev <khasanboyurmonov1998@yahoo.com>
2024-11-07 15:04:58 +0900

    feat: Get Users for Sidebar Endpoint is developed and API is created

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMnogoDB from "./db/connectToMongoDB.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,6 +16,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 /*app.get("/", (req, res) => {
   //root route http://localhost:5000/
   res.send("Hello World!!!");
