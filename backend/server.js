ffcacbf6750315b97aa23a0809a26503ad31373a
khasanboyurmonovdev <khasanboyurmonov1998@yahoo.com>
2024-11-09 14:43:20 +0900

    feat: backend and frontend is now connected and all data go to database

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMnogoDB from "./db/connectToMongoDB.js";
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 
+app.use(cors());
 app.use(express.json()); //to parse the incoming requests with JSON payloads from
 app.use(cookieParser());
 
--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -32,7 +32,7 @@ const NoChatSelected = () => {
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
         <p>Welcome 👋 Luis Nani </p>
         <p> Select a chat to start messaging</p>
-        <TiMessages className="text-3x1 md:text-6x1 text-center" />
+        <TiMessages className="text-3xl md:text-6xl text-center" />
       </div>
     </div>
   );
